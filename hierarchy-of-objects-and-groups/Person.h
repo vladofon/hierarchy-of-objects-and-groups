1517a36512526a1a6899e92cf5415b5d8567a441
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-05-06 21:07:48 +0300

    Create Person class

---
+++ hierarchy-of-objects-and-groups/Person.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Object.h"
+
+using namespace std;
+
+class person : public object
+{
+public:
+
+   string get_name() const
+   {
+      return name_;
+   }
+
+   void set_name(string name)
+   {
+      this->name_ = std::move(name);
+   }
+
+   string to_string() override
+   {
+      return "Person [name=" + name_ + "];";
+   }
+
+private:
+
+   string name_;
+};
